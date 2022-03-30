98b36f01666285a5b91cb3fffb5b1855faea232e
Simone Aronica <simonearonica@protonmail.ch>
2022-03-30 15:22:21 +0200

    Added flexible header

--- src/components/header.js
+++ src/components/header.js
@@ -2,41 +2,37 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
+import './header.scss';
+
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+    className="header"
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    <h1 style={{ margin: 0 }}>
+      <Link
+        to="#"
+      >
+    {
+      siteTitle === Header.defaultProps.siteTitle ? siteTitle.components.map(c => {
+        return <span className={c.className}>{c.text}</span>
+      }) : siteTitle
+    }
+      </Link>
+    </h1>
   </header>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+  siteTitle: PropTypes.object,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: {
+    components: [
+      { text: 'Green', className: 'text-green' },
+      { text: 'print', className: '' }
+    ]
+  },
 }
 
 export default Header
