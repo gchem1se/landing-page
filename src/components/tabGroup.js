2c3c4e74bf86b7d1ee0b175c5c5ed4c186efa5f7
Simone Aronica <simonearonica@protonmail.ch>
2022-03-30 18:26:40 +0200

    Added scrollable

--- src/components/tabGroup.js
+++ src/components/tabGroup.js
@@ -6,16 +6,38 @@ import "./tabGroup.scss"
 
 const TabGroup = ({tabs, className}) => 
 {
-  const [activeTab, setTab] = React.useState(tabs[0]);
-  const setActiveTab = (tab) => {
-    setTab(tab);
+  const [activeTab, setActiveTab] = React.useState(tabs[0]);
+  const coords = React.useRef(0);
+
+  const handleInteractionStart = (e) => {
+    if(e._reactName === "onTouchStart"){
+      coords.current = e.touches[0].clientX;
+    }
+    else if(e._reactName === "onMouseDown"){
+      coords.current = e.clientX;
+    }
   }
-  
-  return <div className={'tab-wrapper' + className}>
+
+  const handleInteractionEnd = (e) => {
+    if(e._reactName === "onTouchEnd"){
+      if(e.touches[0].clientX > coords.current){
+      }else if(e.touches[0].clientX < coords.current){
+        setActiveTab(tabs[(tabs.indexOf(activeTab) + 1) % tabs.length])
+      }
+    }
+    else if(e._reactName === "onMouseUp"){
+      if(e.clientX > coords.current){
+      }else if(e.clientX < coords.current){
+        setActiveTab(tabs[(tabs.indexOf(activeTab) + 1) % tabs.length])
+      }
+    }
+  }
+ 
+  return <div className={'tab-wrapper' + className} onTouchStart={handleInteractionStart} onTouchEnd={handleInteractionEnd} onMouseDown={handleInteractionStart} onMouseUp={handleInteractionEnd}>
     <div className="tabs">
     {
       tabs.map((tab) => (
-        <div className="tab" active={tab === activeTab ? "true" : "false"}>
+        <div className={ "tab " + (tab !== activeTab ? "hide" : "") } active={tab === activeTab ? "true" : "false"}>
         { tab }
         </div>
       )) 
@@ -24,7 +46,7 @@ const TabGroup = ({tabs, className}) =>
     <div className="tab-nav">
     {
       tabs.map((tab) =>(
-        <span className="nav-dot" active={tab === activeTab ? "true" : "false"}>
+        <span className={"nav-dot"} active={tab === activeTab ? "true" : "false"}>
         </span>
       ))
     }
