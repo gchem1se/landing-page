64bd5add59f2f521a73034277f7f9db33a3e6c0d
Simone Aronica <simonearonica@protonmail.ch>
2022-03-30 18:33:13 +0200

    Sync

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Button from "../components/Button"
+import TabGroup from "../components/tabGroup"
 
 import "./page-2.scss"
 
@@ -17,15 +18,16 @@ const SecondPage = () => (
       <Button text={"Act now!"}></Button>
     </section>
     <section className="CTASection">
-      <h3>Se ni' mondo esistesse</h3>
-      <h4>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor
-      </h4>
-      <h3>Be healthy in YOUR environment</h3>
-      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
-      <h3>Go green to get greens</h3>
-      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+      <TabGroup
+        tabs={[
+          <div>
+            <h3>Lorem ipsum dolor sit amet</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            </p>
+          </div>
+        ]}
+      />
     </section>
   </Layout>
 )
